fb449ca6c39eb1b779df30a41dd96a7bb08e81df
visual_community@outlook.com <visual_community@outlook.com>
2019-09-27 20:04:33 -0400

    primer commit

---
+++ Examen1/Examen1/Models/DataContext.cs
@@ -0,0 +1,15 @@
+﻿
+namespace Examen1.Models
+{
+    using System.Data.Entity;
+    public class DataContext : DbContext
+
+    {
+        public DataContext() : base("DafaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<Examen1.Models.Persona> Personas { get; set; }
+    }
+}
---
+++ Examen1/Examen1/Models/Persona.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Examen1.Models
+{
+    public enum TypeLastname
+    {
+        Ingrid,
+        Ivan,
+        Irusta,
+        Iriarte,
+        Ilda,
+
+    }
+    public class Persona
+    {
+        [Key]
+        public int Friendly { get; set; }
+        [Required]
+        public string Name { get; set; }
+
+        public TypeLastname List { get; set; }
+        [Required]
+        public string Email { get; set; }
+
+        public int Birthdate { get; set; }
+    }
+}
